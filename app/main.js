c62f8d669ef1cddda5a93163aee1f0e3600da120
Farhan Khanzada <m.farhankz@gmail.com>
2014-10-20 13:22:43 +0500

    add files

---
+++ app/main.js
@@ -0,0 +1,74 @@
+(function(){
+    angular.module('ShoppingCart', ['ngRoute'])
+
+    .controller('ListController',function($scope,$http){
+        $http.get('app/data.json').success (function(data){
+            $scope.guitarVariable = data[0].products;
+        });
+    })
+    .controller('ListControllerTow',function($scope,$http){
+        $http.get('app/data.json').success (function(data){
+            $scope.guitarVariable = data[0].products;
+        });
+    })
+
+    .controller('ListControllerThree',function($scope,$http){
+        $http.get('app/data.json').success (function(data){
+            $scope.guitarVariable = data[0].products;
+        });
+    })
+    .controller('ListControllerFour',function($scope,$http){
+        $http.get('app/data.json').success (function(data){
+            $scope.guitarVariable = data[0].products;
+        });
+    })
+    .controller('detailCtrl',function($scope,$http){
+        $http.get('app/data.json').success (function(data){
+            $scope.guitarVariable = data[0].products;
+        });
+    })
+
+    .controller('HeaderController',function($scope, $location, $http){
+        $http.get('app/data.json').success (function(data){
+            $scope.siteTitle = data[0].siteTitle;
+        });
+        $http.get('app/data.json').success (function(data){
+            $scope.navigation = data[0].nav;
+        });
+        $http.get('app/data.json').success (function(data){
+            $scope.sidenavigation = data[0].topNav;
+        });
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+     })
+
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/javascript', {
+                    templateUrl: 'app/views/list.html',
+                    controller: 'ListController'
+                }).
+                when('/backbone-js', {
+                    templateUrl: 'app/views/list.html',
+                    controller: 'ListControllerTow'
+                }).
+                when('/angular-js', {
+                    templateUrl: 'app/views/list.html',
+                    controller: 'ListControllerThree'
+                }).
+                when('/node-js', {
+                    templateUrl: 'app/views/list.html',
+                    controller: 'ListControllerFour'
+                }).
+                when('/javascript/b01', {
+                    templateUrl: 'app/views/detail.html',
+                    controller: 'detailCtrl'
+                }).
+                otherwise({
+                    redirectTo: '/javascript'
+                });
+        }]);
+
+})();
