70eb01518b5924d0ed643dbfd4cc8eb80b8e06bb
fkhanzada <@rtde$igner5544>
2014-10-29 18:41:49 +0500

    cart page

--- app/main.js
+++ app/main.js
@@ -2,28 +2,21 @@
     angular.module('ShoppingCart', ['ui.router'])
 
     .controller('FeatureProduct',function($scope,getDataToJson,$location){
-            getDataToJson.getData('app/data.json',function(data){
-                var result = [];
-                var current;
-                for(var i = 0; i < data[0].products.length; i++){
-                    current = data[0].products[i];
-                    if(current.featureProduct === true){
-                        result.push(current);
-                    }
+        getDataToJson.getData('app/data.json',function(data){
+            var result = [];
+            var current;
+            for(var i = 0; i < data[0].products.length; i++){
+                current = data[0].products[i];
+                if(current.featureProduct === true){
+                    result.push(current);
                 }
-                $scope.BookStore = result;
-                $scope.BookStore[0].categoryName = 'Featured';
-            });
-    })
-    .service('getDataToJson', function($http){
-        return {
-            getData: function(url,callBack){
-                $http.get(url).success(callBack);
             }
-        }
+            $scope.BookStore = result;
+            $scope.BookStore[0].categoryName = 'Featured';
+        });
     })
-    .controller('JavaScript',function($scope,$http,$location){
-        $http.get('app/data.json').success (function(data){
+    .controller('JavaScript',function($scope,getDataToJson,$location){
+        getDataToJson.getData('app/data.json',function(data){
             var cat = $location.$$path.split('/')[1];
             var result = [];
             var current;
@@ -36,23 +29,8 @@
             $scope.BookStore = result;
         });
     })
-    .controller('BackboneJs',function($scope,$http,$location){
-        $http.get('app/data.json').success (function(data){
-            var cat = $location.$$path.split('/')[1];
-            var result = [];
-            var current;
-            for(var i = 0; i < data[0].products.length; i++){
-                current = data[0].products[i];
-                if(current.category === cat){
-                    result.push(current);
-                }
-            }
-            $scope.BookStore = result;
-
-        });
-    })
-    .controller('AngularJs',function($scope,$http,$location){
-        $http.get('app/data.json').success (function(data){
+    .controller('BackboneJs',function($scope,getDataToJson,$location){
+        getDataToJson.getData('app/data.json',function(data){
             var cat = $location.$$path.split('/')[1];
             var result = [];
             var current;
@@ -65,8 +43,8 @@
             $scope.BookStore = result;
         });
     })
-    .controller('NodeJs',function($scope,$http,$location){
-        $http.get('app/data.json').success (function(data){
+    .controller('AngularJs',function($scope,getDataToJson,$location){
+        getDataToJson.getData('app/data.json',function(data){
             var cat = $location.$$path.split('/')[1];
             var result = [];
             var current;
@@ -79,8 +57,22 @@
             $scope.BookStore = result;
         });
     })
-    .controller('detailCtrl',function($scope,$http,$location){
-        $http.get('app/data.json').success (function(data){
+    .controller('NodeJs',function($scope,getDataToJson,$location){
+        getDataToJson.getData('app/data.json',function(data){
+            var cat = $location.$$path.split('/')[1];
+            var result = [];
+            var current;
+            for(var i = 0; i < data[0].products.length; i++){
+                current = data[0].products[i];
+                if(current.category === cat){
+                    result.push(current);
+                }
+            }
+            $scope.BookStore = result;
+        });
+    })
+    .controller('detailCtrl',function($scope,getDataToJson,$location){
+        getDataToJson.getData('app/data.json',function(data){
             var id = $location.$$path.split('/')[2];
             var current;
             for(var i = 0; i < data[0].products.length; i++){
@@ -101,55 +93,53 @@
             }
             $scope.BookStore = result;
 
-             setTimeout(function(){
-                 $("#slider").owlCarousel({
-                     navigation : true,
-                     slideSpeed : 300,
-                     paginationSpeed : 400,
-                     singleItem:true
-                 });
-             },50);
-
-
+            setTimeout(function(){
+                $("#slider").owlCarousel({
+                    navigation : true,
+                    slideSpeed : 300,
+                    paginationSpeed : 400,
+                    singleItem:true
+                });
+            },50);
         });
     })
-
-
-    .controller('HeaderController',function($scope, $location, $http){
-        $http.get('app/data.json').success (function(data){
+    .controller('HeaderController',function($scope, getDataToJson,$location){
+        getDataToJson.getData('app/data.json',function(data){
             $scope.siteTitle = data[0].siteTitle;
-        });
-        $http.get('app/data.json').success (function(data){
             $scope.navigation = data[0].nav;
-        });
-        $http.get('app/data.json').success (function(data){
             $scope.sidenavigation = data[0].topNav;
         });
         $scope.isActive = function (viewLocation) {
             return viewLocation === $location.path();
         };
      })
-
     .value('cart', [])
-
-        .controller('AddToCart', function($scope,getDataToJson, cart){
-            getDataToJson.getData('app/data.json',function(data){
-                $scope.addCart = (function(id){
-                    cart.push({id:id});
-                    var result = [];
-                    var current;
-                    for(var i = 0; i < data[0].products.length; i++){
-                        current = data[0].products[i];
-                        if(current.productId === id){
-                            result.push(current);
-                        }
+    .controller('AddToCart', function($scope,getDataToJson, cart){
+        getDataToJson.getData('app/data.json',function(data){
+            var current;
+            $scope.addCart = (function(id){
+                for(var i = 0; i < data[0].products.length; i++){
+                    current = data[0].products[i];
+                    if(current.productId === id){
+                        cart.push(current);
                     }
-                    $scope.cartValue = result;
-                    console.log($scope.cartValue[0].price, $scope.cartValue[0].productId);
-                });
+                }
             });
-        })
+        });
+    })
 
+    .controller('cartUpdate', function($scope, cart){
+        $scope.lenghtItem = cart.length;
+        $scope.priceItem = 0;
+    })
+
+    .service('getDataToJson', function($http){
+        return {
+            getData: function(url,callBack){
+                $http.get(url).success(callBack);
+            }
+        }
+    })
     .config(['$stateProvider','$urlRouterProvider','$locationProvider',
         function($stateProvider, $urlRouterProvider, $locationProvider) {
             //$locationProvider.html5Mode(true);
